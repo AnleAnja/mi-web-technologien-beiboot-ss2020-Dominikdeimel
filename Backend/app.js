2ddb26a7370e197db375b8a7cdf438d4d2c3d89b
Dominik Deimel <domi.deimel@yahoo.de>
2020-04-01 15:48:31 +0200

    Basis express and vue server added

---
+++ Backend/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.raw({
+    type: 'image/png',
+    limit: '10mb'
+}));
+
+
+app.post("/images", function (req,res) {
+    
+});
+
+
+
+app.listen(3000, function(){
+    console.log("Example app listening on port: 3000");
+});
