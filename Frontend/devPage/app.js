10ee73f280e2febf563e23ba9f8066e9e29cf34a
Dominik Deimel <domi.deimel@yahoo.de>
2020-07-14 22:54:04 +0200

    Lint added

--- Frontend/devPage/app.js
+++ Frontend/devPage/app.js
@@ -10,6 +10,7 @@ app.use(cors())
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/index.html'));
 });
+
 app.get('/collection', async function (req, res) {
     try {
         const result = await axios.get('http://backend:3000/api/images/collection?sortBy=color&imageCount=3&sortOrder=ascending')
