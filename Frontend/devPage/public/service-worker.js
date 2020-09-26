286f08c0bf292658ba180084f5b3f0fa90288a59
AnleAnja <anleanja@live.de>
2020-09-26 11:22:22 +0200

    add caching

--- Frontend/devPage/public/service-worker.js
+++ Frontend/devPage/public/service-worker.js
@@ -1,22 +1,58 @@
 const CACHE_NAME = 'service-worker-cache';
+const landscapeReq = new Request('http://192.168.2.106:3000/api/images/single?format=landscape', { headers: {'Accept': 'application/json'} });
+const portraitReq = new Request('http://192.168.2.106:3000/api/images/single?format=portrait', { headers: {'Accept': 'application/json'} });
+const quoteReq = new Request('http://quotes.rest/qod', { headers: {'Accept': 'application/json'} });
+
 const toCache = [
     '/',
     '/js/status.js',
-    '/js/main.js',
-    '/js/pwa.webmanifest'
+    '/js/pwa.webmanifest',
+    '/js/main.min.js',
+    '/css/styles.min.css',
+    '/images/splash-screen.png',
+    '/images/apple-touch.png',
+    'https://fonts.gstatic.com/s/barlow/v5/7cHpv4kjgoGqM7E_DMs5.woff2',
+    'https://fonts.googleapis.com/css?family=Barlow',
+    landscapeReq,
+    portraitReq,
 ];
 
+async function getImages(cache) {
+    try {
+        await cache.add(quoteReq);
+    } catch (e) {
+        await cache.put(quoteReq, new Response(null));
+    }
+
+    await cache.addAll(toCache);
+
+    const landscape = await (await cache.match(landscapeReq)).json();
+    const portrait = await (await cache.match(portraitReq)).json();
+
+    await cache.add(new Request(`http://192.168.2.106:3000${landscape.imagePath}`));
+    await cache.add(new Request(`http://192.168.2.106:3000${portrait.imagePath}`));
+}
+
 self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(toCache))
-            .then(self.skipWaiting())
-    );
+    if (navigator.onLine) {
+        event.waitUntil(
+            caches.delete(CACHE_NAME)
+                .then(() => caches.open(CACHE_NAME))
+                .then(cache => getImages(cache))
+                .then(self.skipWaiting())
+        );
+    }
 });
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         fetch(event.request)
+            .then(async response => {
+                const cache = await caches.open(CACHE_NAME);
+                await cache.delete(event.request);
+                await cache.put(event.request, response);
+                return cache.match(event.request);
+            })
             .catch(() => caches.open(CACHE_NAME)
                 .then((cache) => cache.match(event.request))
             )
@@ -37,3 +73,11 @@ self.addEventListener('activate', function (event) {
             .then(() => self.clients.claim())
     );
 });
+
+setInterval(async () => {
+    console.log('update cache');
+    await caches.delete(CACHE_NAME);
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(toCache);
+    await getImages(cache);
+}, 1000 * 60 * 60 * 24);
